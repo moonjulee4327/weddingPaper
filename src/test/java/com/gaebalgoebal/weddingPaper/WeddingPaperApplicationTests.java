3253b803121b90add13d50b43f7d5f54d14dc8d2
MoonJu Lee <mjlee4327@gw.kaoni.com>
2023-10-31 11:42:55 +0900

    :white_check_mark: add test.

--- src/test/java/com/gaebalgoebal/weddingPaper/WeddingPaperApplicationTests.java
+++ src/test/java/com/gaebalgoebal/weddingPaper/WeddingPaperApplicationTests.java
@@ -1,13 +1,25 @@
 package com.gaebalgoebal.weddingPaper;
 
+import com.gaebalgoebal.weddingPaper.domain.test.controller.ApiTestController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeddingPaperApplicationTests {
 
+	@Autowired
+	private ApiTestController apiTestController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@DisplayName("API 테스트")
+	@Test
+	void 컨텍스트_로딩_테스트(){
+		assert apiTestController != null;
 	}
 
 }
