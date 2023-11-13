19ca9fcf2675a0df9793ad23bcf81f680a66a726
moonju lee <mjlee4327@gmail.com>
2023-11-13 18:43:32 +0900

    :sparkles: add aws S3 Images upload

--- src/main/java/com/gaebalgoebal/weddingPaper/domain/test/controller/ApiTestController.java
+++ src/main/java/com/gaebalgoebal/weddingPaper/domain/test/controller/ApiTestController.java
@@ -1,18 +1,38 @@
 package com.gaebalgoebal.weddingPaper.domain.test.controller;
 
+import com.gaebalgoebal.weddingPaper.global.common.AwsS3Service;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
+@RequiredArgsConstructor
 @Api(tags = "API TEST")
 @RestController
 public class ApiTestController {
 
+    private final AwsS3Service awsS3Service;
+
     @ApiOperation(value = "연동 및 CORS 테스트", notes = "React와 Spring boot 서버 연동을 테스트한다.")
     @GetMapping("/apiTest")
     public String getHelloWorld(){
+        log.info("테스트");
         return "Hello World";
+    }
+
+    @ApiOperation(value = "이미지 업로드 테스트", notes = "aws S3를 통해 이미지를 업로드합니다.")
+    @PostMapping("/imageTest")
+    public String uploadImageTest(@RequestParam("images") MultipartFile[] multipartFiles){
+        for(MultipartFile multipartFile : multipartFiles){
+            awsS3Service.uploadImage(multipartFile);
+        }
+        return "업로드 성공";
     }
 
 }
---
+++ src/main/java/com/gaebalgoebal/weddingPaper/global/common/AwsS3Service.java
@@ -0,0 +1,45 @@
+package com.gaebalgoebal.weddingPaper.global.common;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AwsS3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    private final AmazonS3Client amazonS3Client;
+
+    public String uploadImage(MultipartFile multipartFile){
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
+
+        String fileName = "test1/" + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try(InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(bucketName, fileName, inputStream, objectMetadata);
+        }catch (IOException e){
+            throw new IllegalArgumentException("S3 파일 업로드 실패!");
+        }
+
+        String fileUrl = amazonS3Client.getUrl(bucketName, fileName).toString();
+
+        log.info("file url : " + fileUrl);
+
+        return fileUrl;
+    }
+
+}
